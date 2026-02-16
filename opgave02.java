23c7f223f9315fdeb170b96584fea84322e64b78
Hejsteve <Jonathanvingren@gmail.com>
2026-02-16 18:42:31 +0100

    Add files via upload

---
+++ opgave02.java
@@ -0,0 +1,14 @@
+package Opgaver;
+
+public class opgave02 {
+    static void main(String[] args) {
+
+        int sum = 0;
+        int[] numbers = {4, 6, 7, 2, 3};
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Summen af tallene er: " + sum);
+    }
+}
+
